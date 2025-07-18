104f79c8e4fa313d4736297335d783efb0aa32ab
jtnoble <jtnoble321@gmail.com>
2025-07-18 18:15:37 -0500

    finished icons for current tiles

--- src/images/index.ts
+++ src/images/index.ts
@@ -5,6 +5,45 @@ import pushDragLiftIcon from './weight-lifting-up.png';
 import moveIcon from './leg.png';
 import unarmedStrikeIcon from './high-punch.png';
 import sizeAndStrengthIcon from './muscle-up.png';
+import blindnessIcon from './eyepatch.png';
+import highJumpIcon from './human-cannonball.png';
+import longJumpIcon from './frog-prince.png';
+import difficultTerrainIcon from './light-thorny-triskelion.png';
+import stunnedIcon from './knocked-out-stars.png';
+import searchIcon from './perspective-dice-six-faces-six.png';
+import studyIcon from './sherlock-holmes.png';
+import influenceIcon from './perspective-dice-six.png';
+import utilizeIcon from './rope-coil.png';
+import hideIcon from './spy.png';
+import deafenedIcon from './speaker-off.png';
+import carryCapacityIcon from './robber.png';
+import charmedIcon from './shambling-zombie.png';
+import dodgeIcon from './thrust-bend.png';
+import helpIcon from './shaking-hands.png';
+import improviseIcon from './throwing-ball.png';
+import exhaustedIcon from './tired-eye.png';
+import swimIcon from './water-polo.png';
+import incapacitatedIcon from './wooden-pegleg.png'
+import shortRestIcon from './noodles.png';
+import longRestIcon from './pyre.png';
+import unconsciousIcon from './night-sleep.png';
+import castSpellIcon from './fire-spell-cast.png';
+import restrainedIcon from './handcuffed.png';
+import climbIcon from './hiking.png';
+import deathSavesIcon from './dead-head.png';
+import readyIcon from './box-unpacking.png';
+import objectInteractionIcon from './drop-weapon.png';
+import petrifiedIcon from './aura.png';
+import poisonedIcon from './poison-bottle.png';
+import pronedIcon from './back-pain.png';
+import paralyzedIcon from './bottled-bolt.png';
+import frightenedIcon from './ent-mouth.png';
+import invisibleIcon from './paper-lantern.png';
+import grappledIcon from './quicksand.png';
+import standUpIcon from './sensuousness.png';
+import crawlIcon from './crawl.png';
+import classFeatureIcon from './cultist.png';
+import opportunityAttackIcon from './relationship-bounds.png';
 
 export {
     meleeIcon,
@@ -14,4 +53,43 @@ export {
     moveIcon,
     unarmedStrikeIcon,
     sizeAndStrengthIcon,
+    blindnessIcon,
+    highJumpIcon,
+    longJumpIcon,
+    difficultTerrainIcon,
+    stunnedIcon,
+    searchIcon,
+    studyIcon,
+    influenceIcon,
+    utilizeIcon,
+    hideIcon,
+    deafenedIcon,
+    carryCapacityIcon,
+    charmedIcon,
+    dodgeIcon,
+    helpIcon,
+    improviseIcon,
+    exhaustedIcon,
+    swimIcon,
+    incapacitatedIcon,
+    shortRestIcon,
+    longRestIcon,
+    unconsciousIcon,
+    castSpellIcon,
+    restrainedIcon,
+    climbIcon,
+    deathSavesIcon,
+    readyIcon,
+    objectInteractionIcon,
+    petrifiedIcon,
+    poisonedIcon,
+    pronedIcon,
+    paralyzedIcon,
+    frightenedIcon,
+    invisibleIcon,
+    grappledIcon,
+    standUpIcon,
+    crawlIcon,
+    classFeatureIcon,
+    opportunityAttackIcon,
 };
